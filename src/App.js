55a3eeec5adc2ff799dfdb4f462f8143084c8d47
NathaliaPino <101751308+NathaliaPino@users.noreply.github.com>
2023-05-16 17:39:30 -0300

    #

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import { useState } from "react";
+
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [tarefas, setTarefas] = useState([]);
+
+  function registro (valor){
+    valor.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput('')
+    
+
+  }
+
+
   return (
     <div >
-      <h1>oi</h1>
+      <h1>To-do List</h1>
+
+      <form onSubmit={registro}>
+        <label>Nome da tarefa:</label>
+        <br/>
+        <input placeholder="digite uma tarefa"
+        value = {input}
+        onChange={(valor)=> setInput(valor.target.value)}
+        ></input>
+        <button type="submit">ok</button>
+      </form>
+
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
     </div>
   );
 }
